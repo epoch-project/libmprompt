e0fc78b0469e41d822ce0b1e8aa0e0f4035143fc
Brian Campbell <Brian.Campbell@ed.ac.uk>
2025-06-13 15:51:49 +0100

    Fix a bit of dodgy Morello assembly in a test

--- test/test_mp_async.c
+++ test/test_mp_async.c
@@ -47,7 +47,7 @@ static __noinline void* as_stack_address(void* p) {
 static __noinline void* get_stack_top(void) {
 #if defined(__CHERI__)
   void *sp;
-  __asm__("mov %w0, csp" : "=r" (sp));
+  __asm__("mov %C0, csp" : "=r" (sp));
   return sp;
 #else
   void* top = NULL;
